f9fd204f7e3344149367ab8fdf10fac19596fccd
Sam Yusupov <sergyusup@gmail.com>
2019-07-22 11:57:55 -0400

    Add role Admine to getId

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -6,10 +6,10 @@ import { checkRole } from "../services/chkRole";
   const router = Router();
 
   //Get all users
-  router.get("/users", [checkJwt, checkRole(["Admin", "Manager"])], controllerUsers.listAll);
+  router.get("/users", [checkJwt, checkRole(["Admin","Manager"])], controllerUsers.listAll);
 
   //Get one user
-  router.get("/users/:id([0-9]+)", [checkJwt, checkRole(["Manager","Employee"])], controllerUsers.getOneById);
+  router.get("/users/:id([0-9]+)", [checkJwt, checkRole(["Admin","Manager","Employee"])], controllerUsers.getOneById);
 
   //Create a new user
   router.post("/users/newuser", [checkJwt, checkRole(["Admin"])], controllerUsers.newUser);
