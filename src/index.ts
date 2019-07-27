52616e008fb26759e3beb39454818412a3e0ec75
Sam Yusupov <sergyusup@gmail.com>
2019-07-27 16:24:08 -0400

    deleted cors

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import cookieParser from "cookie-parser";
 import { closePool } from "./services/serviceDb";
 
 // Create a new express application instance
-var token;
 const app = express();
 const port = 5050;
 
@@ -18,7 +17,6 @@ process.on('SIGINT', () => {closePool(); port_handler.close(); });
 
 // Call midlewares
 app.use(cookieParser());
-app.use(cors({exposedHeaders: ['Content-Length', `${token}`, 'X-Foo', 'X-Bar'],}));
 app.use(bodyParser.json());
 app.use(express.json());
 
