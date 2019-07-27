7f15254d70f1d83c646fecebfe066dac6fa4355f
Sam Yusupov <sergyusup@gmail.com>
2019-07-27 19:17:37 -0400

    in progress

--- src/controllers/controllerAuth.ts
+++ src/controllers/controllerAuth.ts
@@ -18,7 +18,7 @@ class controllerAuth {
     bcrypt.compare(payload.password, newUser.password).then(function(match) {
     if(match) {
         const token = jwt.sign({userid: newUser.userid, username: payload.username, role: newUser.role }, JWT.jwtSecret, { expiresIn: "2h" } );
-        res.writeHead(201, { 'Authorization': `Bearer ${token}` }); res.end('Authentication Ok!!!');} else {res.status(404).send("Authentication failed!!!");} 
+        res.append('Authorization', `Bearer ${token}` ); res.end('Authentication Ok!!!');} else {res.status(404).send("Authentication failed!!!");} 
     });   
     } catch (error) {res.status(401).send(`Error!!! ${error}`);};      
   } 
@@ -47,7 +47,7 @@ class controllerAuth {
       }
     );
     const newToken = jwt.sign({userid: newUser.userid, username: newUser.username, role: newUser.role }, JWT.jwtSecret, {expiresIn: "2h"});
-    res.writeHead(201, { 'Authorization': `Bearer ${newToken}` }); res.end('Password was changed!!!');
+    res.append('Authorization', `Bearer ${newToken}` ); res.end('Password was changed!!!');
     }  catch (error) {res.status(401).send(`Error!!! ${error}`);};
 }
 }
