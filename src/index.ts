348a702577b301eb696eee81e5da2551e082a27d
Sam Yusupov <sergyusup@gmail.com>
2019-07-27 16:21:58 -0400

    add token

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import { closePool } from "./services/serviceDb";
 
 // Create a new express application instance
+var token;
 const app = express();
 const port = 5050;
 
@@ -17,7 +18,7 @@ process.on('SIGINT', () => {closePool(); port_handler.close(); });
 
 // Call midlewares
 app.use(cookieParser());
-app.use(cors({exposedHeaders: ['Content-Length', 'auth', 'X-Foo', 'X-Bar'],}));
+app.use(cors({exposedHeaders: ['Content-Length', `${token}`, 'X-Foo', 'X-Bar'],}));
 app.use(bodyParser.json());
 app.use(express.json());
 
