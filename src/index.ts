9cfc7c1f0d5ca4d6ea45e4dd090cc9c6c126e35f
Sam Yusupov <sergyusup@gmail.com>
2019-07-27 23:43:52 -0400

    CORS!!!

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,21 @@ import cors from 'cors';
 import { closePool } from "./services/serviceDb";
 
 // Create a new express application instance
+var whitelist = ['http://localhost:3000']
 const app = express();
 const port = 5050;
 
 // Open port
+
+var corsOptionsDelegate = function (req, callback) {
+var corsOptions;
+if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 const port_handler = app.listen(port, () => { console.log(`ERS running on port ${port}.`);
 
 // Close the pool when app shuts down
@@ -18,9 +29,9 @@ process.on('SIGINT', () => {closePool(); port_handler.close(); });
 // Call midlewares
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.options('*', cors());
+app.use(cors());
 app.use(express.json());
 
 //Set all routes from routes folder
-app.use("/", routes);
+app.use("/", cors(corsOptionsDelegate), routes);
 });
