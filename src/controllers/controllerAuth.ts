c01eabf780b2d2be0dca778366c4612d198ab147
Sam Yusupov <sergyusup@gmail.com>
2019-07-28 15:59:37 -0400

    Some....

--- src/controllers/controllerAuth.ts
+++ src/controllers/controllerAuth.ts
@@ -18,7 +18,7 @@ class controllerAuth {
     bcrypt.compare(payload.password, newUser.password).then(function(match) {
     if(match) {
         const token = jwt.sign({userid: newUser.userid, username: payload.username, role: newUser.role }, JWT.jwtSecret, { expiresIn: "2h" } );
-        res.setHeader('Authorization', `${token}`); res.status(201).send(`${token}`);} else {res.status(404).send("Authentication failed!!!");} 
+        res.setHeader('Authorization', `${token}`); res.status(201).send(`${newUser.roleid}:${token}`);} else {res.status(401).send("Authentication failed!!!");} 
     });
     } catch (error) {res.status(401).send(`Error!!! ${error}`);};      
   } 
@@ -29,7 +29,7 @@ class controllerAuth {
     const username = res.locals.jwtPayload.username;
     const id = res.locals.jwtPayload.userid;
     const {oldPassword, newPassword} = req.body; //Get parameters from the body
-    if (!(oldPassword && newPassword)) {res.status(400).send("Passwords don't match!!!");}
+    if (!(oldPassword && newPassword)) {res.status(401).send("Passwords don't match!!!");}
     const userDB = await Pool.query(`select * from GetUserByName($1)`,[username]); //Get user from the database
     const userData = userDB.rows[0];
     const newUser = new User(userData);
