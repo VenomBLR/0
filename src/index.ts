6c4ced862a0ac4525a0b679e46eee87c7d2283cf
Sam Yusupov <sergyusup@gmail.com>
2019-07-28 00:18:18 -0400

    Cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const port = 5050;
 var corsOptionsDelegate = function (req, callback) {
 var corsOptions;
 if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    corsOptions = { origin: true, maxAge: 86400} // reflect (enable) the requested origin in the CORS response
   } else {
     corsOptions = { origin: false } // disable CORS for this request
   }
