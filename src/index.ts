e0ce979d603ebf5cb520f5544408adc86233e30b
Sam Yusupov <sergyusup@gmail.com>
2019-07-27 22:10:40 -0400

    in progress

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import express from "express"
 import bodyParser from "body-parser"
 import routes from "./routers/main";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { closePool } from "./services/serviceDb";
 
 // Create a new express application instance
 const app = express();
 const port = 5050;
+const corsOptions: cors.CorsOptions = {
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Authorization"],
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    origin: "*",
+    preflightContinue: false
+};
 
 // Open port
 const port_handler = app.listen(port, () => { console.log(`ERS running on port ${port}.`);
@@ -16,22 +24,9 @@ process.on('SIGINT', () => {closePool(); port_handler.close(); });
 
 // Call midlewares
 app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-
-    app.options('*', (req, res) => {
-        // allowed XHR methods  
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-        res.send();
-    });
-});
 
 //Set all routes from routes folder
 app.use("/", routes);
