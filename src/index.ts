0f9d7c8e2554ed87c8a3f20466c569deae1e09e2
Sam Yusupov <sergyusup@gmail.com>
2019-07-27 22:55:26 -0400

    in progress

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import express from "express"
 import bodyParser from "body-parser"
 import routes from "./routers/main";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { closePool } from "./services/serviceDb";
 
 // Create a new express application instance
 const app = express();
 const port = 5050;
+const corsOptions: cors.CorsOptions = {
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Authorization"],
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    origin: "*",
+    preflightContinue: false
+};
 
 // Open port
 const port_handler = app.listen(port, () => { console.log(`ERS running on port ${port}.`);
@@ -16,6 +24,7 @@ process.on('SIGINT', () => {closePool(); port_handler.close(); });
 
 // Call midlewares
 app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(express.json());
 
--- src/services/chkJWT.ts
+++ src/services/chkJWT.ts
@@ -19,10 +19,7 @@ export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
   //We want to send a new token on every request
   const { userid, username, role } = jwtPayload;
   const newToken = jwt.sign({ userid, username, role }, JWT.jwtSecret, {expiresIn: "2h"});
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header('Authorization' , `${newToken}`);
+  res.setHeader('Authorization' , `${newToken}`);
   //Call the next middleware or controller
   next();
 };
